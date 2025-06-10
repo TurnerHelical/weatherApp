f63e88dd3447293868e0ff734212181f2a870fe0
TurnerHelical <hunterde91@gmail.com>
2025-06-10 09:09:20 -0400

    weather API call function is working correctly

---
+++ src/scripts/apiCalls.js
@@ -0,0 +1,27 @@
+class weatherAPI {
+
+    //Call the API and return the response, include a location, and optionally a date1 and date2
+    async weatherCall(location, date1, date2) {
+        try {
+            if (!date1 && !date2) {
+                const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=8R9F9CZ8KCSN2SRRVVYA2NF49`)
+                const weatherData = await response.json();
+                return weatherData;
+            }
+            else if (date1 && !date2) {
+                const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}?key=8R9F9CZ8KCSN2SRRVVYA2NF49`)
+                const weatherDataByDate = await response.json();
+                return weatherDataByDate;
+            } else if (date1 && date2) {
+                const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?key=8R9F9CZ8KCSN2SRRVVYA2NF49`)
+                const weatherDataByDateRange = await response.json();
+                return weatherDataByDateRange;
+            }
+            
+
+        } catch (error) {
+            console.log(error);
+        }
+        };
+    }
+    export {weatherAPI};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,7 @@
 import "../CSS/style.css";
+import { weatherAPI } from "./apiCalls";
+
+const weatherCall = new weatherAPI();
+
+const weatherData = weatherCall.weatherCall(34231);
+console.log(weatherData);
