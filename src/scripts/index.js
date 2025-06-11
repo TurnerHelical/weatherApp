b9f5863bbe6fd9036f3300e6c9d5e1e906e866da
TurnerHelical <hunterde91@gmail.com>
2025-06-11 08:41:26 -0400

    formSubmit function now returns the form as key - value pairs in an object

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,10 @@
 import "../CSS/style.css";
 import { weatherManager } from "./weatherManager"
-
+import { PageAction } from "./page";
 const weather = new weatherManager();
+const page = new PageAction();
+
+page.addListener();
 
 
 
--- src/scripts/page.js
+++ src/scripts/page.js
@@ -4,21 +4,36 @@ const page = new Dom();
 
 class PageAction {
 
-    formSubmit(e) {
-        e.preventDefault();
-        const location = page.findElement('#location');
-        const date1 = page.findElement('#date1')
-        const dateRange1 = page.findElement('#dateRange1');
-        const dateRange2 = page.findElement('#dateRange2');
-        if (location.value && !date1 && !dateRange1 && !dateRange2) {
-            
-        }
+    formSubmit() {
         
+        const form = page.findElement('#userForm');
+        const formData = new FormData(form);
+        const formObj = Object.fromEntries(formData.entries());
+        console.log(formObj);
         
+                    
         
-        else (location.value == '' && date1.value == '' && dateRange1.value == '' && dateRange2.value == '' ) {
-            console.log('Please enter some information')
-        }
     }
+    addListener() {
+        const form = page.findElement('#userForm');
+        form.addEventListener('submit', () => {
+            this.formSubmit();
+        });
+    }
+    //     const location = page.findElement('#location');
+    //     const date1 = page.findElement('#date1')
+    //     const dateRange1 = page.findElement('#dateRange1');
+    //     const dateRange2 = page.findElement('#dateRange2');
+    //     if (location.value && !date1 && !dateRange1 && !dateRange2) {
+
+    //     }
+        
+        
+        
+    //     else (location.value == '' && date1.value == '' && dateRange1.value == '' && dateRange2.value == '' ) {
+    //         console.log('Please enter some information')
+    //     }
+    // }
     
 }
+export {PageAction};
