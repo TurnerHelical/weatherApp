9edaf882a37ea7b1af40edfb40383f86d8228eab
TurnerHelical <hunterde91@gmail.com>
2025-06-16 14:47:01 -0400

    added default image for page load

--- src/scripts/page.js
+++ src/scripts/page.js
@@ -11,7 +11,7 @@ class PageAction {
         const form = page.findElement('#userForm');
         const formData = new FormData(form);
         const formObj = Object.fromEntries(formData.entries());
-        console.log(formObj);
+        
         
         
         try{
